K9100ii/android_hardware_samsung_slsi-linaro_exynos

exynos: add exynos-thermal
 * taken from https://gitlab.com/Linaro/96boards/e850-96/device/samsung/erd850/-/tree/android10-e850-96/exynos-thermal @ d5fdb4c9b38d6aa8f6c73bca7cfce31426fe3d61

Change-Id: I0297f35ef51591ba7354bb13ef8add18bba68277

exynos-thermal/thermal-algo.cpp
@@ -1,0 +1,215 @@
+/*
+* Copyright (C) 2018 The Android Open Source Project
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*      http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+
+#include "exynos-thermal.h"
+
+/* Algoritham base class Implementation */
+Algo::Algo(string &name, unsigned int samplingTime, int _triggerPoint, int _clearPoint, string &scenName, bool isNegative, Sensor *sensor, ThermalThread *thread) :
+name(name), samplingTime(samplingTime), sensor(sensor), thread(thread), isNegative(isNegative) {
+	activeStatus = false;
+	sensor->insertAlgoItems(this, scenName);
+
+	if (isNegative) {
+		triggerPoint = -_triggerPoint;
+		clearPoint = -_clearPoint;
+	}
+	else {
+		triggerPoint = _triggerPoint;
+		clearPoint = _clearPoint;
+	}
+
+}
+
+void Algo::activate(void) {
+	activeStatus = true;
+	thread->activate();
+}
+
+void Algo::deactivate(void) {
+	activeStatus = false;
+	thread->deactivate();
+}
+
+/* Monitor Implemenation */
+Monitor::Monitor(string &name, unsigned int samplingTime, int triggerPoint, int clearPoint, string &scenName, bool isNegative, Sensor *sensor, MonitorThread *thread, vector<vector<Device *>> &device, vector<vector<int>> &actionInfo, vector<int> &_thresholds, vector<int> &_clrThresholds) :
+Algo(name, samplingTime, triggerPoint, clearPoint, scenName, isNegative, sensor, thread), device(device), actionInfo(actionInfo) {
+	if (isNegative) {
+		for (int t : _thresholds)
+			thresholds.push_back(-t);
+		for (int t : _clrThresholds)
+			clrThresholds.push_back(-t);
+	}
+	else {
+		thresholds.resize(_thresholds.size());
+		std::copy(_thresholds.begin(), _thresholds.end(), thresholds.begin());
+		clrThresholds.resize(_clrThresholds.size());
+		std::copy(_clrThresholds.begin(), _clrThresholds.end(), clrThresholds.begin());
+	}
+}
+int Monitor::decision(void){
+	int temp = 0;
+	unsigned int newStatus = 0;
+
+	if (isNegative)
+		temp = -(sensor->getTemp());
+	else
+		temp = sensor->getTemp();
+
+	if (status == 0 || (temp >= thresholds[status] && status < thresholds.size())) {
+		// If the temperature is higher than next status, determine new status from thresholds
+		for (int t : thresholds) {
+			if (t <= temp) {
+				newStatus++;
+				continue;
+			}
+			break;
+		}
+	}
+	else if (temp < clrThresholds[status - 1]) {
+		// If the status cannot go to upper, determine new status from clrThresholds
+		for (int t : clrThresholds) {
+			if (t <= temp) {
+				newStatus++;
+				continue;
+			}
+			break;
+		}
+	}
+	else
+		newStatus = status;
+
+	printlog("[ExynosThermal] Monitor [%s] temp(%d) status(%d -> %d)\n", this->name.c_str(), temp, status, newStatus);
+
+	// If need to apply new status
+	if (newStatus != status) {
+		if (newStatus == 0)
+			deactivate();
+		else {
+			// Release previous limit
+			if (status > 0) {
+				for (Device *d : device[status - 1])
+					d->deviceControl(name, 0);
+			}
+			// Request new limit
+			if (newStatus > 0) {
+				int i = 0;
+				for (Device *d : device[newStatus - 1])
+					d->deviceControl(name, actionInfo[newStatus - 1][i++]);
+			}
+			// Apply final status of devicies
+			if (status > 0) {
+				for (Device *d : device[status - 1])
+					d->statusApply();
+			}
+			if (newStatus > 0) {
+				for (Device *d : device[newStatus - 1])
+					d->statusApply();
+			}
+			status = newStatus;
+		}
+	}
+
+	return 0;
+}
+void Monitor::deactivate(void) {
+	if (status > 0) {
+		for (Device *d : device[status - 1])
+			d->deviceControl(name, 0);
+
+		for (Device *d : device[status - 1])
+			d->statusApply();
+
+		activeStatus = false;
+		status = 0;
+		thread->deactivate();
+	}
+}
+
+/* SS Implemenation */
+SS::SS(string &name, unsigned int samplingTime, int triggerPoint, int clearPoint, int startLevel, string &scenName, bool isNegative, Sensor* sensor, SSThread *thread, Device *device, int devicePerfFloor = 0, int timeTickTrigger = 0) :
+Algo(name, samplingTime, triggerPoint, clearPoint, scenName, isNegative, sensor, thread), startLevel(startLevel), device(device), devicePerfFloor(devicePerfFloor), timeTickTrigger(timeTickTrigger) {}
+int SS::decision(void){
+	int temp = 0;
+	int nextStatus, nextValue, tempDiff, tickTriggered = 0;
+
+	if (isNegative)
+		temp = -(sensor->getTemp());
+	else
+		temp = sensor->getTemp();
+
+	// If temperature is lower then clearPoint, release the thermal restriction.
+	if (temp < clearPoint){
+		printlog("[ExynosThermal] SS [%s] temp(%d->%d) status(%d -> 0)\n", this->name.c_str(), prevTemperature, temp, status);
+		deactivate();
+		return 0;
+	}
+
+	/*
+	 * If status move 0 to upper, calculate tempDiff from triggerPoint.
+	 * If not, calculate the temparture different from prevTemperature.
+	 */
+	if (status == 0)
+		tempDiff = (temp / 1000) - (triggerPoint / 1000) + 1;
+	else
+		tempDiff = (temp / 1000) - (prevTemperature / 1000);
+
+	// If the SS algorithm uses timeTickTrigger, update and apply time tick counter
+	if (timeTickTrigger != 0) {
+		if (tempDiff == 0 && device->getCurrentStatus() <= devicePerfFloor) {
+			// If there has no temperature change, update tickTrigger
+			if (++timeTick == timeTickTrigger)
+				tickTriggered = 1;
+		}
+		else timeTick = 0;
+	}
+
+	nextStatus = status + tempDiff + tickTriggered;
+	if (nextStatus < 0) nextStatus = 0;
+
+	if (nextStatus != status) {
+		status = nextStatus;
+		// Get the raw value from Device
+		nextStatus += startLevel - 1;
+		if (nextStatus >= device->getLevelCount())
+			nextStatus = device->getLevelCount() - 1;
+
+		// Apply new raw value to device
+		nextValue = nextStatus < devicePerfFloor ? nextStatus : devicePerfFloor;
+		printlog("[ExynosThermal] SS [%s] temp(%d->%d) status(%d)\n", this->name.c_str(), prevTemperature, temp, nextValue);
+
+		device->deviceControl(name, nextValue);
+		device->statusApply();
+		timeTick = 0;
+	}
+
+	// Update previous temp
+	prevTemperature = temp;
+
+	return 0;
+}
+
+void SS::activate(void) {
+	activeStatus = true;
+	thread->activate();
+}
+
+void SS::deactivate(void) {
+	timeTick = status = 0;
+	device->deviceControl(name, 0);
+	device->statusApply();
+	activeStatus = false;
+	thread->deactivate();
+}

exynos-thermal/thermal-device.cpp
@@ -1,0 +1,168 @@
+/*
+* Copyright (C) 2018 The Android Open Source Project
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*      http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+
+#include <iomanip>
+#include "exynos-thermal.h"
+
+/* Generic Device Implemenation */
+Device::Device(string &name) : name(name) {}
+Device::Device(string &name, string &nodePath) : name(name), nodePath(nodePath) {
+	node.open(nodePath);
+	if (!node.is_open()) printlog("[ExynosThermal] Device [%s] open failed\n", name.c_str());
+	node << unitbuf; // Flush the write buffer every write
+}
+Device::Device(string &name, string &nodePath, string &levelType, vector<string> &leveltoValue) : name(name), nodePath(nodePath), levelType(levelType), leveltoValue(leveltoValue) {
+	node.open(nodePath);
+	if (!node.is_open()) printlog("[ExynosThermal] Device [%s] open failed\n", name.c_str());
+	node << unitbuf; // Flush the write buffer every write
+}
+
+int Device::getLowestStatus(void) {
+	int i = 0;
+
+	for (auto it = request.begin(); it != request.end(); it++) {
+		if (it->second > i){
+			i = it->second;
+		}
+	}
+	return i;
+}
+
+int Device::deviceControl(const string &algoName, int action) {
+	lock_guard<mutex> LG(lock);
+
+	request[algoName] = action;
+	return 0;
+}
+void Device::statusApply(void) {
+	lock_guard<mutex> LG(lock);
+	int i = 0;
+	string writeVal;
+	int writeInt;
+
+	if (lowestMode == true)
+		i = getLowestStatus();
+
+	if (!leveltoValue.empty())
+		writeVal = leveltoValue[i];
+	else {
+		printlog("[ExynosThermal] %s: Level Table is Empty\n", __func__);
+		return;
+	}
+	if (currentStatus != i) {
+		node.seekp(0, node.beg);
+		if (levelType == "string") {
+			node << writeVal;
+		}
+		else if (levelType == "hex") {
+			writeInt = strtoul(writeVal.replace(0, 2, "").c_str(), NULL, 16);
+			node << std::hex << writeInt;
+		}
+		else if (levelType == "integer") {
+			writeInt = atoi(writeVal.c_str());
+			node << writeInt;
+		}
+
+		currentStatus = i;
+	}
+}
+
+/* Camera Device Implementation */
+void CameraDevice::statusApply(void) {
+	lock_guard<mutex> LG(lock);
+	int i = 0;
+	string writeVal;
+
+	if (lowestMode == true)
+		i = getLowestStatus();
+
+	if (!leveltoValue.empty())
+		writeVal = leveltoValue[i];
+	else {
+		printlog("[ExynosThermal] %s: Level Table is Empty\n", __func__);
+		return;
+	}
+
+	if (currentStatus == 0){
+		ofstream en("/sys/devices/platform/150c0000.exynos_is/debug/en_fixed_sensor_fps");
+		en << unitbuf << "1";
+	}
+	if (i == 0){
+		ofstream en("/sys/devices/platform/150c0000.exynos_is/debug/en_fixed_sensor_fps");
+		en << unitbuf << "0";
+	}
+
+	if (currentStatus != i) {
+		node.seekp(0, node.beg);
+		node << writeVal;
+		currentStatus = i;
+	}
+}
+
+/* CPU Hotplug Device Implementation */
+
+/* Backlight Brightness Device Implementation */
+
+/* CPU Freq Device Implementation */
+void CPUFreqDevice::statusApply(void) {
+	lock_guard<mutex> LG(lock);
+	int i = 0;
+	int writeInt = 0;
+
+	if (lowestMode == true)
+		i = getLowestStatus();
+	if (!leveltoValue.empty())
+		writeInt = atoi(leveltoValue[i].c_str());
+	else {
+		printlog("[ExynosThermal] %s: Level Table is Empty\n", __func__);
+		return;
+	}
+	if (currentStatus != i) {
+		node.seekp(0, node.beg);
+		node.write((const char *)&writeInt, sizeof(writeInt));
+		currentStatus = i;
+	}
+}
+/* GPU Freq Device Implemenation */
+
+/* Charger Device Implementation */
+
+/* Report Device Implementation */
+void ReportDevice::statusApply(void) {
+	lock_guard<mutex> LG(lock);
+	time_t now = chrono::system_clock::to_time_t(chrono::system_clock::now());
+
+	for (auto it = request.begin(); it != request.end(); it++) {
+		if (it->second == 1) {
+			node << put_time(localtime(&now), "%F %T") << " : [" << it->first << "] Over Thresholds, currenct Status(" << it->second << ")" << endl;
+			it->second = 2;
+		}
+		else if (it->second == 0) {
+			node << put_time(localtime(&now), "%F %T") << " : [" << it->first << "] Under Thresholds Clear" << endl;
+			it->second = 2;
+		}
+	}
+}
+
+/* Dummy Device Implementation */
+void DummyDevice::statusApply(void) {
+	lock_guard<mutex> LG(lock);
+
+	int i = getLowestStatus();
+	if (currentStatus != i) {
+		currentStatus = i;
+	}
+}

Evolution-X-Devices/hardware_samsung_slsi-linaro_exynos

exynos-thermal/thermal-algo.cpp
@@ -1,0 +1,215 @@
+/*
+* Copyright (C) 2018 The Android Open Source Project
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*      http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+
+#include "exynos-thermal.h"
+
+/* Algoritham base class Implementation */
+Algo::Algo(string &name, unsigned int samplingTime, int _triggerPoint, int _clearPoint, string &scenName, bool isNegative, Sensor *sensor, ThermalThread *thread) :
+name(name), samplingTime(samplingTime), sensor(sensor), thread(thread), isNegative(isNegative) {
+	activeStatus = false;
+	sensor->insertAlgoItems(this, scenName);
+
+	if (isNegative) {
+		triggerPoint = -_triggerPoint;
+		clearPoint = -_clearPoint;
+	}
+	else {
+		triggerPoint = _triggerPoint;
+		clearPoint = _clearPoint;
+	}
+
+}
+
+void Algo::activate(void) {
+	activeStatus = true;
+	thread->activate();
+}
+
+void Algo::deactivate(void) {
+	activeStatus = false;
+	thread->deactivate();
+}
+
+/* Monitor Implemenation */
+Monitor::Monitor(string &name, unsigned int samplingTime, int triggerPoint, int clearPoint, string &scenName, bool isNegative, Sensor *sensor, MonitorThread *thread, vector<vector<Device *>> &device, vector<vector<int>> &actionInfo, vector<int> &_thresholds, vector<int> &_clrThresholds) :
+Algo(name, samplingTime, triggerPoint, clearPoint, scenName, isNegative, sensor, thread), device(device), actionInfo(actionInfo) {
+	if (isNegative) {
+		for (int t : _thresholds)
+			thresholds.push_back(-t);
+		for (int t : _clrThresholds)
+			clrThresholds.push_back(-t);
+	}
+	else {
+		thresholds.resize(_thresholds.size());
+		std::copy(_thresholds.begin(), _thresholds.end(), thresholds.begin());
+		clrThresholds.resize(_clrThresholds.size());
+		std::copy(_clrThresholds.begin(), _clrThresholds.end(), clrThresholds.begin());
+	}
+}
+int Monitor::decision(void){
+	int temp = 0;
+	unsigned int newStatus = 0;
+
+	if (isNegative)
+		temp = -(sensor->getTemp());
+	else
+		temp = sensor->getTemp();
+
+	if (status == 0 || (temp >= thresholds[status] && status < thresholds.size())) {
+		// If the temperature is higher than next status, determine new status from thresholds
+		for (int t : thresholds) {
+			if (t <= temp) {
+				newStatus++;
+				continue;
+			}
+			break;
+		}
+	}
+	else if (temp < clrThresholds[status - 1]) {
+		// If the status cannot go to upper, determine new status from clrThresholds
+		for (int t : clrThresholds) {
+			if (t <= temp) {
+				newStatus++;
+				continue;
+			}
+			break;
+		}
+	}
+	else
+		newStatus = status;
+
+	printlog("[ExynosThermal] Monitor [%s] temp(%d) status(%d -> %d)\n", this->name.c_str(), temp, status, newStatus);
+
+	// If need to apply new status
+	if (newStatus != status) {
+		if (newStatus == 0)
+			deactivate();
+		else {
+			// Release previous limit
+			if (status > 0) {
+				for (Device *d : device[status - 1])
+					d->deviceControl(name, 0);
+			}
+			// Request new limit
+			if (newStatus > 0) {
+				int i = 0;
+				for (Device *d : device[newStatus - 1])
+					d->deviceControl(name, actionInfo[newStatus - 1][i++]);
+			}
+			// Apply final status of devicies
+			if (status > 0) {
+				for (Device *d : device[status - 1])
+					d->statusApply();
+			}
+			if (newStatus > 0) {
+				for (Device *d : device[newStatus - 1])
+					d->statusApply();
+			}
+			status = newStatus;
+		}
+	}
+
+	return 0;
+}
+void Monitor::deactivate(void) {
+	if (status > 0) {
+		for (Device *d : device[status - 1])
+			d->deviceControl(name, 0);
+
+		for (Device *d : device[status - 1])
+			d->statusApply();
+
+		activeStatus = false;
+		status = 0;
+		thread->deactivate();
+	}
+}
+
+/* SS Implemenation */
+SS::SS(string &name, unsigned int samplingTime, int triggerPoint, int clearPoint, int startLevel, string &scenName, bool isNegative, Sensor* sensor, SSThread *thread, Device *device, int devicePerfFloor = 0, int timeTickTrigger = 0) :
+Algo(name, samplingTime, triggerPoint, clearPoint, scenName, isNegative, sensor, thread), device(device), devicePerfFloor(devicePerfFloor), timeTickTrigger(timeTickTrigger), startLevel(startLevel) {}
+int SS::decision(void){
+	int temp = 0;
+	int nextStatus, nextValue, tempDiff, tickTriggered = 0;
+
+	if (isNegative)
+		temp = -(sensor->getTemp());
+	else
+		temp = sensor->getTemp();
+
+	// If temperature is lower then clearPoint, release the thermal restriction.
+	if (temp < clearPoint){
+		printlog("[ExynosThermal] SS [%s] temp(%d->%d) status(%d -> 0)\n", this->name.c_str(), prevTemperature, temp, status);
+		deactivate();
+		return 0;
+	}
+
+	/*
+	 * If status move 0 to upper, calculate tempDiff from triggerPoint.
+	 * If not, calculate the temparture different from prevTemperature.
+	 */
+	if (status == 0)
+		tempDiff = (temp / 1000) - (triggerPoint / 1000) + 1;
+	else
+		tempDiff = (temp / 1000) - (prevTemperature / 1000);
+
+	// If the SS algorithm uses timeTickTrigger, update and apply time tick counter
+	if (timeTickTrigger != 0) {
+		if (tempDiff == 0 && device->getCurrentStatus() <= devicePerfFloor) {
+			// If there has no temperature change, update tickTrigger
+			if (++timeTick == timeTickTrigger)
+				tickTriggered = 1;
+		}
+		else timeTick = 0;
+	}
+
+	nextStatus = status + tempDiff + tickTriggered;
+	if (nextStatus < 0) nextStatus = 0;
+
+	if (nextStatus != status) {
+		status = nextStatus;
+		// Get the raw value from Device
+		nextStatus += startLevel - 1;
+		if (nextStatus >= device->getLevelCount())
+			nextStatus = device->getLevelCount() - 1;
+
+		// Apply new raw value to device
+		nextValue = nextStatus < devicePerfFloor ? nextStatus : devicePerfFloor;
+		printlog("[ExynosThermal] SS [%s] temp(%d->%d) status(%d)\n", this->name.c_str(), prevTemperature, temp, nextValue);
+
+		device->deviceControl(name, nextValue);
+		device->statusApply();
+		timeTick = 0;
+	}
+
+	// Update previous temp
+	prevTemperature = temp;
+
+	return 0;
+}
+
+void SS::activate(void) {
+	activeStatus = true;
+	thread->activate();
+}
+
+void SS::deactivate(void) {
+	timeTick = status = 0;
+	device->deviceControl(name, 0);
+	device->statusApply();
+	activeStatus = false;
+	thread->deactivate();
+}

exynos-thermal/thermal-device.cpp
@@ -1,0 +1,168 @@
+/*
+* Copyright (C) 2018 The Android Open Source Project
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*      http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+
+#include <iomanip>
+#include "exynos-thermal.h"
+
+/* Generic Device Implemenation */
+Device::Device(string &name) : name(name) {}
+Device::Device(string &name, string &nodePath) : name(name), nodePath(nodePath) {
+	node.open(nodePath);
+	if (!node.is_open()) printlog("[ExynosThermal] Device [%s] open failed\n", name.c_str());
+	node << unitbuf; // Flush the write buffer every write
+}
+Device::Device(string &name, string &nodePath, string &levelType, vector<string> &leveltoValue) : name(name), nodePath(nodePath), leveltoValue(leveltoValue), levelType(levelType) {
+	node.open(nodePath);
+	if (!node.is_open()) printlog("[ExynosThermal] Device [%s] open failed\n", name.c_str());
+	node << unitbuf; // Flush the write buffer every write
+}
+
+int Device::getLowestStatus(void) {
+	int i = 0;
+
+	for (auto it = request.begin(); it != request.end(); it++) {
+		if (it->second > i){
+			i = it->second;
+		}
+	}
+	return i;
+}
+
+int Device::deviceControl(const string &algoName, int action) {
+	lock_guard<mutex> LG(lock);
+
+	request[algoName] = action;
+	return 0;
+}
+void Device::statusApply(void) {
+	lock_guard<mutex> LG(lock);
+	int i = 0;
+	string writeVal;
+	int writeInt;
+
+	if (lowestMode == true)
+		i = getLowestStatus();
+
+	if (!leveltoValue.empty())
+		writeVal = leveltoValue[i];
+	else {
+		printlog("[ExynosThermal] %s: Level Table is Empty\n", __func__);
+		return;
+	}
+	if (currentStatus != i) {
+		node.seekp(0, node.beg);
+		if (levelType == "string") {
+			node << writeVal;
+		}
+		else if (levelType == "hex") {
+			writeInt = strtoul(writeVal.replace(0, 2, "").c_str(), NULL, 16);
+			node << std::hex << writeInt;
+		}
+		else if (levelType == "integer") {
+			writeInt = atoi(writeVal.c_str());
+			node << writeInt;
+		}
+
+		currentStatus = i;
+	}
+}
+
+/* Camera Device Implementation */
+void CameraDevice::statusApply(void) {
+	lock_guard<mutex> LG(lock);
+	int i = 0;
+	string writeVal;
+
+	if (lowestMode == true)
+		i = getLowestStatus();
+
+	if (!leveltoValue.empty())
+		writeVal = leveltoValue[i];
+	else {
+		printlog("[ExynosThermal] %s: Level Table is Empty\n", __func__);
+		return;
+	}
+
+	if (currentStatus == 0){
+		ofstream en("/sys/devices/platform/150c0000.exynos_is/debug/en_fixed_sensor_fps");
+		en << unitbuf << "1";
+	}
+	if (i == 0){
+		ofstream en("/sys/devices/platform/150c0000.exynos_is/debug/en_fixed_sensor_fps");
+		en << unitbuf << "0";
+	}
+
+	if (currentStatus != i) {
+		node.seekp(0, node.beg);
+		node << writeVal;
+		currentStatus = i;
+	}
+}
+
+/* CPU Hotplug Device Implementation */
+
+/* Backlight Brightness Device Implementation */
+
+/* CPU Freq Device Implementation */
+void CPUFreqDevice::statusApply(void) {
+	lock_guard<mutex> LG(lock);
+	int i = 0;
+	int writeInt = 0;
+
+	if (lowestMode == true)
+		i = getLowestStatus();
+	if (!leveltoValue.empty())
+		writeInt = atoi(leveltoValue[i].c_str());
+	else {
+		printlog("[ExynosThermal] %s: Level Table is Empty\n", __func__);
+		return;
+	}
+	if (currentStatus != i) {
+		node.seekp(0, node.beg);
+		node.write((const char *)&writeInt, sizeof(writeInt));
+		currentStatus = i;
+	}
+}
+/* GPU Freq Device Implemenation */
+
+/* Charger Device Implementation */
+
+/* Report Device Implementation */
+void ReportDevice::statusApply(void) {
+	lock_guard<mutex> LG(lock);
+	time_t now = chrono::system_clock::to_time_t(chrono::system_clock::now());
+
+	for (auto it = request.begin(); it != request.end(); it++) {
+		if (it->second == 1) {
+			node << put_time(localtime(&now), "%F %T") << " : [" << it->first << "] Over Thresholds, currenct Status(" << it->second << ")" << endl;
+			it->second = 2;
+		}
+		else if (it->second == 0) {
+			node << put_time(localtime(&now), "%F %T") << " : [" << it->first << "] Under Thresholds Clear" << endl;
+			it->second = 2;
+		}
+	}
+}
+
+/* Dummy Device Implementation */
+void DummyDevice::statusApply(void) {
+	lock_guard<mutex> LG(lock);
+
+	int i = getLowestStatus();
+	if (currentStatus != i) {
+		currentStatus = i;
+	}
+}